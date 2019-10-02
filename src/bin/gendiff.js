3493afd99a4ea3f554640bba0f6d82506863f3d9
korol <cx.korol@gmail.com>
2019-10-02 14:35:40 +0300

    feat: add arguments

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,4 +7,5 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'Output format')
+  .arguments('<firstConfig>, <secondConfig>')
   .parse(process.argv);
