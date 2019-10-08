18e8379fec5edb9014eecc7320340b29eb78b017
korol <cx.korol@gmail.com>
2019-10-08 16:35:48 +0300

    feat: add plain format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,18 +3,19 @@ import path from 'path';
 import gendiff from '../src';
 
 const testFiles = [
-  ['before-tree.json', 'after-tree.json', 'result-tree.txt'],
-  ['before-tree.yml', 'after-tree.yml', 'result-tree.txt'],
+  ['json', 'before-tree.json', 'after-tree.json', 'result-tree.txt'],
+  ['json', 'before-tree.yml', 'after-tree.yml', 'result-tree.txt'],
+  ['plain', 'before-tree.json', 'after-tree.json', 'result-plain.txt'],
 ];
 
 test.each(testFiles)(
   'gendiff(%s, %s)',
-  (before, after, result) => {
+  (format, before, after, result) => {
     const pathToBeforeFile = path.resolve(__dirname, `__fixtures__/${before}`);
     const pathToAfterFile = path.resolve(__dirname, `__fixtures__/${after}`);
     const pathToRefenseFile = path.resolve(__dirname, `__fixtures__/${result}`);
     const reference = fs.readFileSync(pathToRefenseFile, 'utf-8').trim();
 
-    expect(gendiff(pathToBeforeFile, pathToAfterFile)).toEqual(reference);
+    expect(gendiff(pathToBeforeFile, pathToAfterFile, format)).toEqual(reference);
   },
 );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import parse from './parcer';
 import makeAst from './ast';
-import render from './render';
+import getRenderer from './renderers';
 
-const genDiff = (file1, file2) => {
+const genDiff = (file1, file2, format) => {
   const object1 = parse(file1);
   const object2 = parse(file2);
   const ast = makeAst(object1, object2);
+  const render = getRenderer(format);
   const result = render(ast);
   return result;
 };
